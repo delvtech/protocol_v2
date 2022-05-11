0105e6956c929cbc5ae4833745970ec4f6ef7e4a
aleph_v <20227705+aleph-v@users.noreply.github.com>
2022-05-12 00:50:32 +0200

    Reconfigs files to setup workflows (#7)
    * moves into workfows
    
    * fixes for this version
    
    * fixes for ci
    
    * remove outdated sim
    
    Co-authored-by: Paul Vienhage <paul@0x.org>

--- contracts/ForwarderFactory.sol
+++ contracts/ForwarderFactory.sol
@@ -13,8 +13,8 @@ contract ForwarderFactory is IForwarderFactory {
 
     // The transient state variables used in deployment
     // Note - It saves us a bit of gas to not fully zero them at any point
-    IMultiToken token = IMultiToken(address(1));
-    uint256 tokenId = 1;
+    IMultiToken private _token = IMultiToken(address(1));
+    uint256 private _tokenId = 1;
 
     // For reference
     bytes32 public constant ERC20LINK_HASH =
@@ -24,26 +24,26 @@ contract ForwarderFactory is IForwarderFactory {
 
     /// @notice Uses create2 to deploy a forwarder at a predictable address as part of
     ///         our ERC20 multitoken implementation.
-    /// @param _token The multitoken which the forwarder should link to
-    /// @param _tokenId The id of the sub token from the multitoken which we are creating
+    /// @param token The multitoken which the forwarder should link to
+    /// @param tokenId The id of the sub token from the multitoken which we are creating
     ///                 an interface for.
     /// @return returns the address of the deployed forwarder
-    function create(IMultiToken _token, uint256 _tokenId)
+    function create(IMultiToken token, uint256 tokenId)
         external
         returns (ERC20Forwarder)
     {
         // Set the transient state variables before deploy
-        tokenId = _tokenId;
-        token = _token;
+        _tokenId = tokenId;
+        _token = token;
         // The salt is the _tokenId hashed with the multi token
-        bytes32 salt = keccak256(abi.encode(_token, _tokenId));
+        bytes32 salt = keccak256(abi.encode(token, tokenId));
         // Deploy using create2 with that salt
         ERC20Forwarder deployed = new ERC20Forwarder{ salt: salt }();
         // As a consistency check we check that this is in the right address
-        assert(address(deployed) == getForwarder(_token, _tokenId));
+        assert(address(deployed) == getForwarder(token, tokenId));
         // Reset the transient state
-        token = IMultiToken(address(1));
-        tokenId = 1;
+        _token = IMultiToken(address(1));
+        _tokenId = 1;
         // return the deployed forwarder
         return (deployed);
     }
@@ -51,20 +51,20 @@ contract ForwarderFactory is IForwarderFactory {
     /// @notice Returns the transient storage of this contract
     /// @return Returns the stored multitoken address and the sub token id
     function getDeployDetails() external view returns (IMultiToken, uint256) {
-        return (token, tokenId);
+        return (_token, _tokenId);
     }
 
     /// @notice Helper to calculate expected forwarder contract addresses
-    /// @param _token The multitoken which the forwarder should link to
-    /// @param _tokenId The id of the sub token from the multitoken
+    /// @param token The multitoken which the forwarder should link to
+    /// @param tokenId The id of the sub token from the multitoken
     /// @return The expected address of the forwarder
-    function getForwarder(IMultiToken _token, uint256 _tokenId)
+    function getForwarder(IMultiToken token, uint256 tokenId)
         public
         view
         returns (address)
     {
         // Get the salt and hash to predict the address
-        bytes32 salt = keccak256(abi.encode(_token, _tokenId));
+        bytes32 salt = keccak256(abi.encode(token, tokenId));
         bytes32 addressBytes = keccak256(
             abi.encodePacked(bytes1(0xff), address(this), salt, ERC20LINK_HASH)
         );
