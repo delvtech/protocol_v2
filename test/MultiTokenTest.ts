a86ea968aba0d4b8561339d04ed50984b4ddf298
Sarah Mackey <sarahmackey42@gmail.com>
2022-06-13 09:52:16 -0700

    change test to 3rd party signer

--- test/MultiTokenTest.ts
+++ test/MultiTokenTest.ts
@@ -368,7 +368,7 @@ describe("MultiToken Tests", async () => {
       impersonate(wallet.address);
       const walletSigner = ethers.provider.getSigner(wallet.address);
       const tx = erc20
-        .connect(walletSigner)
+        .connect(signers[0])
         .permit(
           wallet.address,
           erc20.address,
