4a954455d344ace6122733ead62a2807a192a286
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2022-06-30 16:47:36 -0500

    FixedPointMathLib: Add efficient pow method (#19)
    * add pow and pow2
    
    * implement new pow method and compare to Balancer's
    
    * minor cleanup and add author credits
    
    * add credit to Balancer for Error code
    
    * fixed constant naming cconvention
    
    * fix linting issues
    
    * added bride of frankenstein pow method and improved tests
    
    * remove old pow methods and cleanup
    
    * fix comment blocks
    
    * update comments
    
    * remove Exponent type and printouts

---
+++ contracts/mocks/MockFixedPointMath.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: Apache-2.0
+pragma solidity ^0.8.15;
+
+import "hardhat/console.sol";
+import "contracts/libraries/TypedFixedPointMathLib.sol";
+
+contract MockFixedPointMath {
+    function pow(UFixedPoint x, UFixedPoint y)
+        public
+        view
+        returns (UFixedPoint result)
+    {
+        uint256 startGas = gasleft();
+        result = TypedFixedPointMathLib.pow(x, y);
+        console.log("gasUsed", startGas - gasleft());
+    }
+}
