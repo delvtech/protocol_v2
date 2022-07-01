f5eff0b4b622bf53e1f125fe71a4162bad6ef966
jonny rhea <jonathan.rhea@gmail.com>
2022-07-01 18:35:55 -0500

    add seperate mock pow function for typed library

--- contracts/mocks/MockFixedPointMath.sol
+++ contracts/mocks/MockFixedPointMath.sol
@@ -3,9 +3,17 @@ pragma solidity ^0.8.15;
 
 import "hardhat/console.sol";
 import "contracts/libraries/TypedFixedPointMathLib.sol";
+import "contracts/libraries/FixedPointMathLib.sol";
 
 contract MockFixedPointMath {
-    function pow(UFixedPoint x, UFixedPoint y)
+    function pow(uint256 x, uint256 y) public view returns (uint256 result) {
+        uint256 startGas = gasleft();
+        result = FixedPointMathLib.pow(x, y);
+        console.log(result);
+        console.log("gasUsed", startGas - gasleft());
+    }
+
+    function powTyped(UFixedPoint x, UFixedPoint y)
         public
         view
         returns (UFixedPoint result)
