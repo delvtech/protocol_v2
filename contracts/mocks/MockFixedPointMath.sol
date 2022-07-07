a175d85aa3169bff245a3fd0e062bc0a6ecfe4b2
Jonny Rhea <5555162+jrhea@users.noreply.github.com>
2022-07-07 09:17:19 -0500

    Yield space math lib (#25)
    * Added YieldSpaceMathLib with basic tests
    
    * add natspec comments to YieldSpaceMathLib methods
    
    * removed commented out code
    
    * fix typo in comment
    
    * more comment cleanup
    
    * factor out an extra multDown call
    
    * re-implement calculateShareOutGivenBondIn() formula to take less operations
    
    * combined the two YieldSpaceMath functions into one
    
    * create add() sub() tests
    
    * remove console.sol
    
    * pull calculation for k out of the conditional

--- contracts/mocks/MockFixedPointMath.sol
+++ contracts/mocks/MockFixedPointMath.sol
@@ -6,6 +6,18 @@ import "contracts/libraries/TypedFixedPointMathLib.sol";
 import "contracts/libraries/FixedPointMathLib.sol";
 
 contract MockFixedPointMath {
+    function add(uint256 x, uint256 y) public view returns (uint256 result) {
+        uint256 startGas = gasleft();
+        result = FixedPointMathLib.add(x, y);
+        console.log("gasUsed", startGas - gasleft());
+    }
+
+    function sub(uint256 x, uint256 y) public view returns (uint256 result) {
+        uint256 startGas = gasleft();
+        result = FixedPointMathLib.sub(x, y);
+        console.log("gasUsed", startGas - gasleft());
+    }
+
     function pow(uint256 x, uint256 y) public view returns (uint256 result) {
         uint256 startGas = gasleft();
         result = FixedPointMathLib.pow(x, y);
@@ -31,6 +43,26 @@ contract MockFixedPointMath {
     {
         uint256 startGas = gasleft();
         result = TypedFixedPointMathLib.pow(x, y);
+        console.log("gasUsed", startGas - gasleft());
+    }
+
+    function addTyped(UFixedPoint x, UFixedPoint y)
+        public
+        view
+        returns (UFixedPoint result)
+    {
+        uint256 startGas = gasleft();
+        result = TypedFixedPointMathLib.add(x, y);
+        console.log("gasUsed", startGas - gasleft());
+    }
+
+    function subTyped(UFixedPoint x, UFixedPoint y)
+        public
+        view
+        returns (UFixedPoint result)
+    {
+        uint256 startGas = gasleft();
+        result = TypedFixedPointMathLib.sub(x, y);
         console.log("gasUsed", startGas - gasleft());
     }
 }
