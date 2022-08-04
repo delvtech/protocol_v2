a6cf0b6ac5425d495587e42dbda123413bab2156
Pádraic Ó Mhuiris <patrick.morris.310@gmail.com>
2022-08-04 14:35:16 +0100

    update

--- test/foundry/CompoundV3Term.t.sol
+++ test/foundry/CompoundV3Term.t.sol
@@ -15,79 +15,18 @@ import "@compoundV3/contracts/test/SimplePriceFeed.sol";
 import "@compoundV3/contracts/CometConfiguration.sol";
 import "@compoundV3/contracts/CometStorage.sol";
 
-contract MockCompoundV3 is Comet {
-    address _auth = address(0xb4c79daB8f259C7Aee6E5b2Aa729821864227e84); // foundry test deployer
-
-    constructor(
-        address _baseToken,
-        address _baseTokenPriceFeed,
-        AssetConfig[] memory _assetConfigs
-    )
-        /// https://github.com/compound-finance/comet/blob/main/deployments/kovan/configuration.json
-        Comet(
-            Configuration({
-                governor: _auth,
-                pauseGuardian: _auth,
-                baseToken: _baseToken,
-                baseTokenPriceFeed: _baseTokenPriceFeed,
-                extensionDelegate: address(0x0),
-                supplyKink: 800000000000000000, // 0.8
-                supplyPerYearInterestRateSlopeLow: 32500000000000000, // 0.0325
-                supplyPerYearInterestRateSlopeHigh: 400000000000000000, // 0.4
-                supplyPerYearInterestRateBase: 0,
-                borrowKink: 800000000000000000, // 0.8
-                borrowPerYearInterestRateSlopeLow: 35000000000000000, // 0.035
-                borrowPerYearInterestRateSlopeHigh: 250000000000000000, // 0.25
-                borrowPerYearInterestRateBase: 15000000000000000,
-                storeFrontPriceFactor: 500000000000000000, // 0.5
-                trackingIndexScale: 1000000000000000, //
-                baseTrackingSupplySpeed: 11574074074,
-                baseTrackingBorrowSpeed: 1145833333333,
-                baseMinForRewards: 1000000000000, // 100k USDC
-                baseBorrowMin: 1000000000, // 1000 USDC
-                targetReserves: 5000000000000, // 500K USDC
-                assetConfigs: _assetConfigs
-            })
+library CompoundV3TermHelper {
+    // Deploys both Compound and CompoundV3Term contracts and sets up an
+    // emulated live scenario
+    function createCompound(Vm vm)
+        public
+        returns (
+            Comet compound,
+            CompoundV3Term term,
+            MockERC20Permit USDC,
+            MockERC20Permit WETH
         )
-    {}
-}
-
-contract CompoundV3TermTest is Test {
-    CompoundV3Term public term;
-
-    MockCompoundV3 public compound;
-
-    MockERC20Permit public USDC;
-
-    MockERC20Permit public WETH;
-    SimplePriceFeed public priceFeed_WETH;
-
-    address public Alice = vm.addr(0xA11CE);
-    address public Bob = vm.addr(0xB0B);
-    address public Carol = vm.addr(0xCA701);
-
-    uint256 public amountCollateral;
-    uint256 public amountDebt;
-    uint256 public amountSupply;
-
-    uint256 public constant MAX_RESERVE = 50000e18;
-    uint256 public constant YEAR = (365 * 24 * 60 * 60);
-
-    uint256 public TERM_START;
-    uint256 public TERM_END;
-
-    function labels() public {
-        vm.label(Alice, "Alice");
-        vm.label(Bob, "Bob");
-        vm.label(Carol, "Carol");
-        vm.label(address(compound), "Compound");
-        vm.label(address(USDC), "USDC");
-        vm.label(address(WETH), "WETH");
-    }
-
-    function setUp() public {
-        ForwarderFactory forwarderFactory = new ForwarderFactory();
-
+    {
         USDC = new MockERC20Permit("USDC Coin", "USDC", 6);
         WETH = new MockERC20Permit("Wrapped Ether", "WETH", 18);
 
@@ -96,6 +35,7 @@ contract CompoundV3TermTest is Test {
 
         CometConfiguration.AssetConfig[]
             memory assetConfigs = new CometConfiguration.AssetConfig[](1);
+
         assetConfigs[0] = CometConfiguration.AssetConfig({
             asset: address(WETH),
             priceFeed: address(priceFeed_WETH),
@@ -106,77 +46,112 @@ contract CompoundV3TermTest is Test {
             supplyCap: 1000000e18
         });
 
-        compound = new MockCompoundV3(
-            address(USDC),
-            address(priceFeed_USDC),
-            assetConfigs
-        );
+        CometConfiguration.Configuration memory cometConfig = CometConfiguration
+            .Configuration({
+                governor: msg.sender,
+                pauseGuardian: msg.sender,
+                baseToken: address(USDC),
+                baseTokenPriceFeed: address(priceFeed_USDC),
+                extensionDelegate: address(0x0),
+                supplyKink: 0.8e18, // 0.8
+                supplyPerYearInterestRateSlopeLow: 0.0325e18,
+                supplyPerYearInterestRateSlopeHigh: 0.4e18,
+                supplyPerYearInterestRateBase: 0,
+                borrowKink: 0.8e18,
+                borrowPerYearInterestRateSlopeLow: 0.035e18,
+                borrowPerYearInterestRateSlopeHigh: 0.25e18,
+                borrowPerYearInterestRateBase: 0.15e18,
+                storeFrontPriceFactor: 0.5e18,
+                trackingIndexScale: 1e15,
+                baseTrackingSupplySpeed: 11574074074074073,
+                baseTrackingBorrowSpeed: 11458333333333333,
+                baseMinForRewards: 100000e6,
+                baseBorrowMin: 1000e6,
+                targetReserves: 5000000e6,
+                assetConfigs: assetConfigs
+            });
+
+        compound = new Comet(cometConfig);
+
+        ForwarderFactory forwarderFactory = new ForwarderFactory();
 
         term = new CompoundV3Term(
             address(compound),
             forwarderFactory.ERC20LINK_HASH(),
             address(forwarderFactory),
-            MAX_RESERVE,
+            100_000e6,
             address(this)
         );
 
         compound.initializeStorage();
 
-        amountSupply = 10000000e6;
-        amountDebt = 8000000e6;
-        amountCollateral = 10000e18;
+        address Alice = vm.addr(0xA11CE);
+        address Bob = vm.addr(0xB0B);
 
         vm.deal(Alice, 100 ether);
         vm.deal(Bob, 100 ether);
-        vm.deal(Carol, 100 ether);
 
-        USDC.mint(Alice, amountSupply);
-        WETH.mint(Bob, amountCollateral);
-        USDC.mint(Bob, 1_000_000e6);
-        USDC.mint(Carol, 100000e6);
-
-        labels();
+        USDC.mint(Alice, 10000000e6);
+        USDC.mint(Bob, 1000000e6);
+        WETH.mint(Bob, 10000e18);
 
         vm.startPrank(Alice);
         USDC.approve(address(compound), type(uint256).max);
         WETH.approve(address(compound), type(uint256).max);
-        compound.supply(address(USDC), amountSupply);
+        compound.supply(address(USDC), 10000000e6);
         vm.stopPrank();
 
         vm.startPrank(Bob);
         USDC.approve(address(term), type(uint256).max);
         WETH.approve(address(compound), type(uint256).max);
-        compound.supply(address(WETH), amountCollateral);
-        compound.withdraw(address(USDC), amountDebt);
+        compound.supply(address(WETH), 10000e18);
+        compound.withdraw(address(USDC), 8000000e6);
         vm.stopPrank();
+    }
+}
 
-        // Advance a year in time to accrue some yield
+contract CompoundV3TermTest is Test {
+    CompoundV3Term public lockedTerm;
+
+    Comet public compound;
+
+    MockERC20Permit public USDC;
+    MockERC20Permit public WETH;
+
+    address public user = vm.addr(0xFEED_DEAD_BEEF);
+
+    uint256 public constant YEAR = (365 * 24 * 60 * 60);
+
+    uint256 public TERM_START;
+    uint256 public TERM_END;
+
+    function labels() public {
+        vm.label(user, "User");
+        vm.label(address(compound), "Compound");
+        vm.label(address(term), "Term");
+        vm.label(address(USDC), "USDC");
+        vm.label(address(WETH), "WETH");
+    }
+
+    function setUp() public {
+        (compound, term, USDC, WETH) = CompoundV3TermHelper.create(vm);
+
+        labels();
+
         vm.warp(block.timestamp + YEAR);
 
-        vm.startPrank(Carol);
+        USDC.mint(user, 1_000_000e6);
+        USDC.mint(address(this), 1_000_000e6);
+        vm.deal(user, 100 ether);
+
+        vm.startPrank(user);
         USDC.approve(address(term), type(uint256).max);
         vm.stopPrank();
 
         TERM_START = block.timestamp;
         TERM_END = TERM_START + YEAR;
 
-        vm.startPrank(Bob);
-        uint256[] memory assetIds;
-        uint256[] memory assetAmounts;
-
-        term.lock(
-            assetIds,
-            assetAmounts,
-            10_000e6,
-            false,
-            Bob,
-            Bob,
-            TERM_START,
-            TERM_END
-        );
-        vm.stopPrank();
-
-        // Move halfway through the term
+        // Move halfway through the term so underlying and yieldShares are not 1:1
         vm.warp(block.timestamp + YEAR / 2);
     }
 
@@ -227,7 +202,7 @@ contract CompoundV3TermTest is Test {
         // console2.log(term.underlyingAsYieldShares(10000e6));
         // console2.log(term.yieldSharesIssued());
 
-        vm.startPrank(Carol);
+        vm.startPrank(user);
         uint256[] memory assetIds;
         uint256[] memory assetAmounts;
         term.lock(
@@ -235,8 +210,8 @@ contract CompoundV3TermTest is Test {
             assetAmounts,
             10000e6,
             false,
-            Carol,
-            Carol,
+            user,
+            user,
             TERM_START,
             TERM_END
         );
@@ -246,14 +221,14 @@ contract CompoundV3TermTest is Test {
         // console2.log(term.underlyingAsYieldShares(10000e6));
         // console2.log(term.yieldSharesIssued());
 
-        vm.startPrank(Carol);
+        vm.startPrank(user);
         term.lock(
             assetIds,
             assetAmounts,
             10000e6,
             false,
-            Carol,
-            Carol,
+            user,
+            user,
             TERM_START,
             TERM_END
         );
