5976359513a16fae6599aef07855ca9ba6a70eb7
Alex Towle <jalextowle@gmail.com>
2022-09-12 16:02:59 -0500

    Updated documentation

--- test/foundry/units/TermTest.t.sol
+++ test/foundry/units/TermTest.t.sol
@@ -20,7 +20,6 @@ contract TermTest is Test {
         // Set up the required Element contracts.
         _factory = new ForwarderFactory();
         _underlying = new MockERC20Permit("Test", "TEST", 18);
-        // FIXME: Consider making a user to be the owner.
         _term = new MockTerm(
             _factory.ERC20LINK_HASH(),
             address(_factory),
@@ -29,14 +28,31 @@ contract TermTest is Test {
         );
     }
 
+    // FIXME: Remove this.
+
+    function testGenerateTestMatrix() public {
+        uint256[] memory inputs = new uint256[](3);
+        inputs[0] = 0;
+        inputs[1] = 1;
+        inputs[2] = 2;
+        uint256[][] memory testMatrix = Utils.generateTestingMatrix(4, inputs);
+
+        console.log("[");
+        for (uint256 i = 0; i < testMatrix.length; i++) {
+            console.log("    ", testMatrix[i][1]);
+        }
+        console.log("]");
+    }
+
     // -------------------  _releasePT unit tests   ------------------ //
 
     function testCombinatorialReleasePT() public {
         // Get the test cases.
-        uint256[] memory inputs = new uint256[](3);
+        uint256[] memory inputs = new uint256[](4);
         inputs[0] = 0;
         inputs[1] = 1 ether;
         inputs[2] = 2 ether;
+        inputs[3] = 3.7435 ether;
         ReleasePTTestCase[] memory testCases = convertToReleasePTTestCase(
             Utils.generateTestingMatrix(6, inputs)
         );
@@ -54,7 +70,7 @@ contract TermTest is Test {
                 interest: testCases[i].interest
             });
             _term.setSharesPerExpiry(assetId, testCases[i].sharesPerExpiry);
-            _term.setUnderlyingReturnValue(testCases[i].underlying);
+            _term.setUnderlyingReturnValue(testCases[i].currentPricePerShare);
             _term.setUserBalance(assetId, user, testCases[i].userBalance);
             _term.setTotalSupply(assetId, testCases[i].totalSupply);
 
@@ -64,15 +80,27 @@ contract TermTest is Test {
             if (expectedError.length > 0) {
                 console.log("failure test case ");
                 console.log("");
-                console.log("    amount          = ", testCases[i].amount);
-                console.log("    interest        = ", testCases[i].interest);
+                console.log("    amount               = ", testCases[i].amount);
                 console.log(
-                    "    sharesPerExpiry = ",
+                    "    interest             = ",
+                    testCases[i].interest
+                );
+                console.log(
+                    "    sharesPerExpiry      = ",
                     testCases[i].sharesPerExpiry
                 );
-                console.log("    totalSupply     = ", testCases[i].totalSupply);
-                console.log("    underlying      = ", testCases[i].underlying);
-                console.log("    userBalance     = ", testCases[i].userBalance);
+                console.log(
+                    "    totalSupply          = ",
+                    testCases[i].totalSupply
+                );
+                console.log(
+                    "    currentPricePerShare = ",
+                    testCases[i].currentPricePerShare
+                );
+                console.log(
+                    "    userBalance          = ",
+                    testCases[i].userBalance
+                );
                 console.log("");
 
                 vm.expectRevert(expectedError);
@@ -85,15 +113,27 @@ contract TermTest is Test {
             } else {
                 console.log("success test case ");
                 console.log("");
-                console.log("    amount          = ", testCases[i].amount);
-                console.log("    interest        = ", testCases[i].interest);
+                console.log("    amount               = ", testCases[i].amount);
                 console.log(
-                    "    sharesPerExpiry = ",
+                    "    interest             = ",
+                    testCases[i].interest
+                );
+                console.log(
+                    "    sharesPerExpiry      = ",
                     testCases[i].sharesPerExpiry
                 );
-                console.log("    totalSupply     = ", testCases[i].totalSupply);
-                console.log("    underlying      = ", testCases[i].underlying);
-                console.log("    userBalance     = ", testCases[i].userBalance);
+                console.log(
+                    "    totalSupply          = ",
+                    testCases[i].totalSupply
+                );
+                console.log(
+                    "    currentPricePerShare = ",
+                    testCases[i].currentPricePerShare
+                );
+                console.log(
+                    "    userBalance          = ",
+                    testCases[i].userBalance
+                );
                 console.log("");
 
                 (uint256 shares, uint256 value) = _term.releasePTExternal(
@@ -107,16 +147,25 @@ contract TermTest is Test {
         }
     }
 
-    // FIXME: Add documentation to this structure and the test suite at large.
     struct ReleasePTTestCase {
+        // The amount of PT to release.
         uint256 amount;
+        // The current price of a single share in the term.
+        uint256 currentPricePerShare;
+        // TODO: Make sure we test that all of the interest is consumed by
+        //       withdrawals of the total supply of YT and PT.
+        //
+        // The amount of underlying backing the PT and YT after finalization.
         uint128 interest;
+        // The amount of shares outstanding for the term.
         uint256 sharesPerExpiry;
+        // The total supply of PT.
         uint256 totalSupply;
-        uint256 underlying;
+        // The user's balance of PT.
         uint256 userBalance;
     }
 
+    // Converts a raw testing matrix to a structured array.
     function convertToReleasePTTestCase(uint256[][] memory rawTestCases)
         internal
         pure
@@ -131,22 +180,28 @@ contract TermTest is Test {
             testCases[i] = ReleasePTTestCase({
                 amount: rawTestCases[i][0],
                 interest: uint128(rawTestCases[i][1]),
-                sharesPerExpiry: rawTestCases[i][2],
-                totalSupply: rawTestCases[i][3],
-                underlying: rawTestCases[i][4],
+                currentPricePerShare: rawTestCases[i][2],
+                sharesPerExpiry: rawTestCases[i][3],
+                totalSupply: rawTestCases[i][4],
                 userBalance: rawTestCases[i][5]
             });
         }
     }
 
+    // Given a test case, get the expected error that will be thrown by a failed
+    // call to _releasePT.
     function getExpectedErrorReleasePT(ReleasePTTestCase memory testCase)
         internal
         pure
         returns (bytes memory)
     {
-        if (testCase.underlying == 0) {
+        if (testCase.currentPricePerShare == 0) {
             return stdError.divisionError;
-        } else if (testCase.interest != 0 && testCase.sharesPerExpiry == 0) {
+        } else if (
+            (testCase.interest * 1e18) / testCase.currentPricePerShare >
+            testCase.sharesPerExpiry
+        ) {
+            // TODO: Re-evaluate this case in the context of _releaseYT.
             return stdError.arithmeticError;
         } else if (testCase.totalSupply == 0) {
             return stdError.divisionError;
@@ -159,6 +214,8 @@ contract TermTest is Test {
         return new bytes(0);
     }
 
+    // Given a test case, validate the state transitions and return values of a
+    // successful call to _releasePT.
     function validateReleasePTSuccess(
         ReleasePTTestCase memory testCase,
         uint256 assetId,
@@ -168,10 +225,11 @@ contract TermTest is Test {
         // Ensure that the calculated shares and value are correct.
         uint256 expectedPTShares = testCase.sharesPerExpiry -
             (testCase.interest * 1e18) /
-            testCase.underlying;
+            testCase.currentPricePerShare;
         uint256 expectedShares = (expectedPTShares * testCase.amount) /
             testCase.totalSupply;
-        uint256 expectedValue = (expectedShares * testCase.underlying) / 1e18;
+        uint256 expectedValue = (expectedShares *
+            testCase.currentPricePerShare) / 1e18;
         assertEq(shares, expectedShares);
         assertEq(value, expectedValue);
 
