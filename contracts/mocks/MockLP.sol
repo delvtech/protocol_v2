ee36594e438751a786050b3fccdb93b2ab061af8
Matthew Brown <mattfdbrown@gmail.com>
2022-10-04 10:01:28 -0500

    Add Stubs for MockLP and MockTerm (#127)

--- contracts/mocks/MockLP.sol
+++ contracts/mocks/MockLP.sol
@@ -5,18 +5,12 @@ import "contracts/LP.sol";
 import "contracts/interfaces/IERC20.sol";
 
 contract MockLP is LP {
-    uint256 internal _lpShares;
-
     constructor(
         IERC20 _token,
         ITerm _term,
         bytes32 _linkerCodeHash,
         address _factory
     ) LP(_token, _term, _linkerCodeHash, _factory) {} // solhint-disable-line no-empty-blocks
-
-    function setLpShares(uint256 value) public {
-        _lpShares = value;
-    }
 
     function setTotalSupply(uint256 poolId, uint256 value) public {
         totalSupply[poolId] = value;
@@ -38,6 +32,9 @@ contract MockLP is LP {
         _mint(poolId, owner, value);
     }
 
+    // ##############################
+    // ###   _depositFromShares   ###
+    // ##############################
     event DepositFromShares(
         uint256 poolId,
         uint256 currentShares,
@@ -46,6 +43,12 @@ contract MockLP is LP {
         uint256 pricePerShare,
         address to
     );
+
+    uint256 internal _depositFromSharesReturnValue;
+
+    function setDepositFromSharesReturnValue(uint256 value) public {
+        _depositFromSharesReturnValue = value;
+    }
 
     // use this to stub calls to _depositFromShares
     function _depositFromShares(
@@ -64,7 +67,7 @@ contract MockLP is LP {
             pricePerShare,
             to
         );
-        return _lpShares;
+        return _depositFromSharesReturnValue;
     }
 
     // use this to test the actual _depositFromShares method
@@ -86,4 +89,40 @@ contract MockLP is LP {
                 to
             );
     }
+
+    // #############################
+    // ###   _withdrawToShares   ###
+    // #############################
+    event WithdrawToShares(uint256 poolId, uint256 amount, address source);
+
+    uint256 internal _withdrawSharesValue;
+    uint256 internal _withdrawBondsValue;
+
+    function setWithdrawToSharesReturnValues(
+        uint256 sharesValue,
+        uint256 bondsValue
+    ) public {
+        _withdrawSharesValue = sharesValue;
+        _withdrawBondsValue = bondsValue;
+    }
+
+    // use this to stub calls to _withdrawFromShares
+    function _withdrawToShares(
+        uint256 poolId,
+        uint256 amount,
+        address source
+    ) internal override returns (uint256, uint256) {
+        emit WithdrawToShares(poolId, amount, source);
+        return (_withdrawSharesValue, _withdrawBondsValue);
+    }
+
+    // use this to test the actual _withdrawToShares method
+    function withdrawToSharesExternal(
+        uint256 poolId,
+        uint256 amount,
+        address source
+    ) external returns (uint256, uint256) {
+        return super._withdrawToShares(poolId, amount, source);
+    }
+    // ------------------------- //
 }
